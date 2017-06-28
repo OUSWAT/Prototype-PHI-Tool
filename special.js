c2dee9b737d671445ee087c59140b855c485bd4f
ckarstens <karstens.chris@gmail.com>
2017-06-28 14:58:43 -0500

    test edit

--- special.js
+++ special.js
@@ -1,3 +1,14 @@
+       	/*
+
+       	Copyright (c) 2017 Cooperative Institute for Mesoscale Meteorological Studies (CIMMS) and the University of Oklahoma
+
+       	Author:         Dr. Chris Karstens
+       	Email:          chris.karstens@noaa.gov
+       	Description:   
+
+       	*/
+
+
 function specialConfig(){
 	return;
 }
